658f000cc6be748cf6db92f1b8de908fc51e16f8
David-Byun <byundavid@naver.com>
2022-02-05 16:22:56 +0900

    Movie app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import Button from "./Button";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <div>
-      <h1>Welcome back!!</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
